db6f89412c301fedeb7ad17e934cd486bfd54792
Sebastian Jambor <seb.jambor@gmail.com>
2016-12-24 12:25:13 +0100

    move constants to top of file

--- src/walks-map.js
+++ src/walks-map.js
@@ -2,7 +2,16 @@ import React from 'react';
 import { Map, Polyline, TileLayer } from 'react-leaflet';
 import walks from './walks';
 
-const position = [52.45, 13.30];
+const POSITION = [52.45, 13.30];
+const TILE_URL = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+const TILE_ATTRIBUTION =
+  'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
+  ' <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' +
+  ' Imagery © <a href="http://mapbox.com">Mapbox</a>';
+const ACCESS_TOKEN =
+  'pk.eyJ1IjoiY3JlcGVscyIsImEiOiJjaXdheGxpdTcwMDF2MnpvNmNucDhrdnN0In0.WiXElc_RJUWKB_CFqssrBA';
+const TILE_ID = 'mapbox.outdoors';
+
 
 class Walk extends React.Component {
   constructor() {
@@ -44,15 +53,6 @@ function distance(walks) {
 }
 
 
-const TILE_URL = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
-const TILE_ATTRIBUTION =
-  'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors,' +
-  ' <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>,' +
-  ' Imagery © <a href="http://mapbox.com">Mapbox</a>';
-const ACCESS_TOKEN =
-  'pk.eyJ1IjoiY3JlcGVscyIsImEiOiJjaXdheGxpdTcwMDF2MnpvNmNucDhrdnN0In0.WiXElc_RJUWKB_CFqssrBA';
-const TILE_ID = 'mapbox.outdoors';
-
 class WalksMap extends React.Component {
   constructor() {
     super();
@@ -77,7 +77,7 @@ class WalksMap extends React.Component {
 
     return (
       <div>
-        <Map id='mapid' center={position} zoom={9}>
+        <Map id='mapid' center={POSITION} zoom={9}>
           <TileLayer
             url={TILE_URL}
             attribution={TILE_ATTRIBUTION}
