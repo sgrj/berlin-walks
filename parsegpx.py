7dab4c849664b43784c6495e988a10224b21f2a6
Sebastian Jambor <seb.jambor@gmail.com>
2016-12-25 13:18:59 +0100

    extract names, date, etc. in python parser

--- parsegpx.py
+++ parsegpx.py
@@ -2,15 +2,28 @@
 
 import xml.etree.ElementTree as ET
 import sys
+import re
+import datetime
 
 ns = {'ns': 'http://www.topografix.com/GPX/1/1'}
 
 if __name__ == '__main__':
-    tree = ET.parse(sys.argv[1])
+    gpx_file = sys.argv[1]
+    title = sys.argv[2]
+    distance = sys.argv[3]
+    name_string = sys.argv[4]
+    names = re.findall(r'\b[A-Z][a-zA-Z]+', name_string)
+    names.append('Lisa')
+    names = ["'" + name + "'" for name in sorted(names)]
+
+    tree = ET.parse(gpx_file)
     root = tree.getroot()
 
     trk = root.find('ns:trk', ns)
     trksegs = trk.findall('ns:trkseg', ns)
+
+    date_string = trksegs[0].find('ns:trkpt', ns).find('ns:time', ns).text
+    date = datetime.datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%SZ")
 
     result = []
     for seg in trksegs:
@@ -20,9 +33,10 @@ if __name__ == '__main__':
 
     print '{'
     print '  path: [' + ', '.join(result) + '],'
-    print "  title: '',"
-    print "  distance: '',"
-    print "  date: '',"
+    print "  title: '" + title + "',"
+    print "  distance: " + distance + ","
+    print "  date: '" + date.strftime('%d %b %Y') + "',"
     print "  participants: ["
+    print "    " + ',\n    '.join(names)
     print "  ]"
     print "},"
