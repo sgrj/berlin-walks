0bca139ab0984de6650a8622db51e1d14574d0f2
Sebastian Jambor <seb.jambor@gmail.com>
2016-12-25 16:54:49 +0100

    extracting WalkDetails

---
+++ src/walk-details.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import _ from 'lodash';
+import './walk-details.css';
+
+function WalkDetails(props) {
+  if (props.walk == null) {
+    return (
+      <div className='walk-details empty'>
+        <div className='title'>Select a walk</div>
+      </div>
+    );
+  }
+
+  const separator = ' – ';
+
+  const participatsLinks = props.walk.participants.map(participant =>
+    <a
+      className='participant'
+      onClick={() => props.onClickName(participant)}
+      key={participant}>
+      {participant}
+    </a>
+  );
+
+  const participantsList = _.range(participatsLinks.length * 2 - 1).map(function(i) {
+    return i % 2 === 0 ? participatsLinks[i / 2] : ' • ';
+  });
+
+  return (
+    <div className='walk-details'>
+      <div className='title'>{props.walk.title}</div>
+      <div className='info'>
+        <span className='date'>{props.walk.date}</span>
+        {separator}
+        <span className='distance'>{props.walk.distance} km</span>
+        {separator}
+        <span className='participants'>
+          {props.walk.participants.length} walkers
+        </span>
+      </div>
+      <div className='details'>
+        {participantsList}
+      </div>
+    </div>
+  );
+}
+
+export default WalkDetails;
--- src/walks-map.js
+++ src/walks-map.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Map, TileLayer } from 'react-leaflet';
-import _ from 'lodash';
 
 import WalkPath from './walk-path';
+import WalkDetails from './walk-details';
 
 
 const POSITION = [52.45, 13.30];
@@ -25,48 +25,6 @@ function distance(walks) {
   }, 0);
 }
 
-function WalkDetails(props) {
-  if (props.walk == null) {
-    return (
-      <div className='walk-details empty'>
-        <div className='title'>Select a walk</div>
-      </div>
-    );
-  }
-
-  const separator = ' – ';
-
-  const participatsLinks = props.walk.participants.map(participant =>
-    <a
-      className='participant'
-      onClick={() => props.onClickName(participant)}
-      key={participant}>
-      {participant}
-    </a>
-  );
-
-  const participantsList = _.range(participatsLinks.length * 2 - 1).map(function(i) {
-    return i % 2 === 0 ? participatsLinks[i / 2] : ' • ';
-  });
-
-  return (
-    <div className='walk-details'>
-      <div className='title'>{props.walk.title}</div>
-      <div className='info'>
-        <span className='date'>{props.walk.date}</span>
-        {separator}
-        <span className='distance'>{props.walk.distance} km</span>
-        {separator}
-        <span className='participants'>
-          {props.walk.participants.length} walkers
-        </span>
-      </div>
-      <div className='details'>
-        {participantsList}
-      </div>
-    </div>
-  );
-}
 
 function GlobalInfo(props) {
   const totalDistance = distance(props.walks);
