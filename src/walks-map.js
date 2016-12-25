37cc7c63c56f9a6306aec35c321ca9e0a06d8ead
Sebastian Jambor <seb.jambor@gmail.com>
2016-12-25 16:05:00 +0100

    don't add \'s after s

--- src/walks-map.js
+++ src/walks-map.js
@@ -110,7 +110,9 @@ function GlobalInfo(props) {
   const totalDistance = distance(props.walks);
   const totalWalks = props.walks.length;
 
-  const displayName = props.name ? props.name + '\'s' : 'Berlin';
+  const displayName = props.name ?
+    props.name + '\'' + (props.name.endsWith('s') ? '' : 's') :
+    'Berlin';
 
   const prefix = props.name ? props.name + ' walked ' : '';
 
